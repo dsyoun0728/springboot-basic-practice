564b6e48998974f9743b4efe30d798e491b96c6d
DESKTOP-6VD51LK\dongsub_youn <dongsub_youn@tmax.co.kr>
2022-09-26 17:32:10 +0900

    [chore] Ch.08 Spring Data JPA 활용 init project

---
+++ ch08_advanced_jpa/src/main/java/com/springboot/advanced_jpa/data/dao/ProductDao.java
@@ -0,0 +1,15 @@
+package com.springboot.advanced_jpa.data.dao;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+
+public interface ProductDao {
+
+  Product insertProduct(Product product);
+
+  Product selectProduct(Long number);
+
+  Product updateProductName(Long number, String name) throws Exception;
+
+  void deleteProduct(Long number) throws Exception;
+
+}
